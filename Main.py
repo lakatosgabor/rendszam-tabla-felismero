8a5bd50cc44693fb9639e9908fa11c322ca95e21
Gabor Lakatos <65727400+lakatosgabor@users.noreply.github.com>
2023-01-27 12:40:45 +0100

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,55 @@
+import cv2
+import DetectChars
+import DetectPlates
+
+SCALAR_BLACK = (0.0, 0.0, 0.0)
+SCALAR_WHITE = (255.0, 255.0, 255.0)
+SCALAR_YELLOW = (0.0, 255.0, 255.0)
+SCALAR_GREEN = (0.0, 255.0, 0.0)
+SCALAR_RED = (0.0, 0.0, 255.0)
+
+showSteps = False
+camera = cv2.VideoCapture(0)
+
+
+def main():
+
+    blnKNNTrainingSuccessful = DetectChars.loadKNNDataAndTrainKNN()
+
+    if blnKNNTrainingSuccessful == False:
+        print("\nerror: KNN traning was not successful\n")
+        return
+    # end if
+
+    while True:
+        success, imgOriginalScene = camera.read()
+
+        if imgOriginalScene is not None:
+            listOfPossiblePlates = DetectPlates.detectPlatesInScene(imgOriginalScene)
+            listOfPossiblePlates = DetectChars.detectCharsInPlates(listOfPossiblePlates)
+
+            if len(listOfPossiblePlates) != 0:
+                listOfPossiblePlates.sort(key = lambda possiblePlate: len(possiblePlate.strChars), reverse = True)
+                licPlate = listOfPossiblePlates[0]
+                print("\nAzonosítva= " + licPlate.strChars + "\n")
+
+                if len(licPlate.strChars) != 0:
+                    if licPlate.strChars == "RAP235":
+                        print("\nAzonosítva= " + licPlate.strChars + "\n")
+            else:
+                print("\nNincs detektálható rendszám!" "\n")
+
+        key = cv2.waitKey(1)
+        if key == ord("q"):
+            break
+
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+
+
+
+
+
